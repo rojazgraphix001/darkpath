2dd245259f8571ebb3d77e2e217489d04c3f577c
R codes <rojagraphix0@gmail.com>
2026-07-26 11:54:28 +0300

    Add files via upload

---
+++ settings (1).js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Dark path 🤖',
+  author: 'Roja Graphix',
+  botName: "DARK PATH-MD",
+  botOwner: 'Roja Graphix', // Your name
+  ownerNumber: '256781631700', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: '', // add your giphy key here 
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/rojazgraphix001/Dark-path/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
